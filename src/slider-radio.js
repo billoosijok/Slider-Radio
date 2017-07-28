93bff8c36dd8f056a3e16ba5114d4667173ce779
Belal Sejouk <billoosijok@gmail.com>
2017-07-28 12:23:48 -0700

    Update README.md

--- src/slider-radio.js
+++ src/slider-radio.js
@@ -17,76 +17,73 @@ window.addEventListener('load', function() {
 
 		// Simply setting up the `sliderPiece` by clicking any pre-checked value;
 		const radios = sliderRadio.querySelectorAll("input[type='radio']");
-		radios.forEach(function(el) { if(el.checked) el.click() });
+		for(let i = 0; i < radios.length; i++) {if(radios[i].checked) radios[i].click() }
+	}
 
-		function handleLabelClick(e) {
-			let label;
-			let radio;
-			if (e.target.nodeName === "INPUT") {
+	function handleLabelClick(e) {
+		let label;
+		let radio;
+		let slidingPiece;
 
-				radio = e.target;
-				label = radio.nextSibling;
+		if (e.target.nodeName === "INPUT") {
 
-				if(radio.checked) {
-					const labelCoords = getElementCoords(label);
+			radio = e.target;
+			label = radio.nextSibling;
+			slidingPiece = radio.parentNode.querySelector(".sliding-piece");
 
-					// Read function description below.
-					runUglyAnimationSetups();
+			if(radio.checked) {
+				const labelCoords = getElementCoords(label);
 
-					animateSlidingPiece(labelCoords);
+				// Read function description below.
+				runUglyAnimationSetups(slidingPiece, );
 
-					if (timeout) {
-						clearTimeout(timeout);
-					}
-					timeout = setTimeout(function() {
-						clearUglyAnimationSetups();
+				animateSlidingPiece(labelCoords);
 
-						timeout = null;
-					}, (animationDuration+0.3) * 1000);
-
-					// window.addEventListener('resize', updateSlidingPieceCoords.bind(sliderRadio, label));
+				if (timeout) {
+					clearTimeout(timeout);
 				}
+				timeout = setTimeout(function() {
+					clearUglyAnimationSetups();
 
-			}
+					timeout = null;
+				}, (animationDuration+0.3) * 1000);
 
-			function updateSlidingPieceCoords(label) {
-				animateSlidingPiece(getElementCoords(label));
-			}
-			option-1
-			option-2
-			option-3
-			option-4
-			option-5
-			function animateSlidingPiece(coords) {
-				slidingPiece.style.left = coords.x + "px";
-				slidingPiece.style.top = coords.y + "px";
-				slidingPiece.style.width = coords.width + "px";
-				slidingPiece.style.height = coords.height + "px";
-			}
-
-			/**
-			 * This function tries to hide the checked label until the animation
-			 * is over, because, in truth, the label is checked instantly. So
-			 * it gets all the styles from the css. But things like border or background-color
-			 * we don't want to show up instantly. So in this function we hide those styles.
-			 */
-			function runUglyAnimationSetups() {
-
-				slidingPiece.style['transition-duration'] = animationDuration;
-				slidingPiece.style.opacity = 1;
-				label.style.backgroundColor = "transparent";
-				label.style.boxShadow = "none";
-				label.style.borderColor = "transparent";
-			}
-
-			function clearUglyAnimationSetups() {
-				label.style.backgroundColor = "";
-				slidingPiece.style.opacity = '0';
-				label.style.boxShadow = "";
-				label.style.borderColor = "";
 			}
 
 		}
+
+		function updateSlidingPieceCoords(label) {
+			animateSlidingPiece(getElementCoords(label));
+		}
+
+		function animateSlidingPiece(coords) {
+			slidingPiece.style.left = coords.x + "px";
+			slidingPiece.style.top = coords.y + "px";
+			slidingPiece.style.width = coords.width + "px";
+			slidingPiece.style.height = coords.height + "px";
+		}
+
+		/**
+		 * This function tries to hide the checked label until the animation
+		 * is over, because, in truth, the label is checked instantly. So
+		 * it gets all the styles from the css. But things like border or background-color
+		 * we don't want to show up instantly. So in this function we hide those styles.
+		 */
+		function runUglyAnimationSetups() {
+			slidingPiece.style['transition-duration'] = animationDuration;
+			slidingPiece.style.opacity = 1;
+			label.style.backgroundColor = "transparent";
+			label.style.boxShadow = "none";
+			label.style.borderColor = "transparent";
+		}
+
+		function clearUglyAnimationSetups() {
+			label.style.backgroundColor = "";
+			slidingPiece.style.opacity = '0';
+			label.style.boxShadow = "";
+			label.style.borderColor = "";
+		}
+
 	}
 
 	function getElementCoords(el) {
